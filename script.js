76f42d2350c1678930eb1eb2e126246570d7a6a9
ronakkumawatgtm147928 <ronakkumawat66795@gmail.com>
2026-02-26 10:19:05 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function bookNow() {
+    alert("Redirecting to Booking Page...");
+    window.location.href = "booking.html";
+}
+
+function submitForm() {
+    alert("Booking Confirmed! Thank You.");
+    return false;
+}
+
+function loginUser() {
+    alert("Login Successful!");
+    return false;
+}
